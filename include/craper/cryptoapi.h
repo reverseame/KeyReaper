c0f2e0607592e2c7e3ebd75800250b6d216819fe
Leon <labascal@unizar.es>
2025-01-10 03:37:59 +0100

    Adapted alignement to 64-bit

--- include/craper/cryptoapi.h
+++ include/craper/cryptoapi.h
@@ -23,16 +23,19 @@ const std::vector<std::string> cryptoapi_function_names = {
 
 struct key_data_s {
   void *unknown;     // +0x00
-  ALG_ID alg;        // +0x04
-  uint32_t flags;    // +0x08
-  uint32_t key_size; // +0x0C
-  void* key_bytes;   // +0x10
-  BYTE unk1[0x10];
-  BYTE iv[0x10];     // +0x24
+  ALG_ID alg;        // +0x04 | +0x08
+  uint32_t flags;    // +0x08 | +0x12
+  uint32_t key_size; // +0x0C | +0x10
+  void* key_bytes;   // +0x10 | +0x18 (padding)
+  void* unk_ptr1;    // alignment for 32 and 64-bit
+  BYTE unk1[0x0C];
+  BYTE iv[0x10];     // +0x24 | +0x34
   BYTE unk2[0x34];
-  DWORD cipher_mode; // +0x68
-  BYTE unk3[0x14];
-  DWORD block_len;   // +0x80
+  DWORD cipher_mode; // +0x68 | +0x78
+  uint32_t unk_align;
+  void* unk_ptr2;
+  BYTE unk3[0x0C];
+  DWORD block_len;   // +0x80 | +0x94
 };
 
 struct magic_s {
