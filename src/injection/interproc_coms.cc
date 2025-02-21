c8f4e65b8e08bc0fc91492370c5d2db24fda5727
Leon Abascal <labascal@unizar.es>
2025-02-21 17:10:13 +0100

    (with previous)

--- src/injection/interproc_coms.cc
+++ src/injection/interproc_coms.cc
@@ -1,9 +1,11 @@
-#include "injection/interproc_coms.h"
-
 #include <iostream>
-#include <wincrypt.h>
 #include <chrono>
 #include <thread>
+#include <nng/nng.h>
+#include <nng/protocol/reqrep0/rep.h>
+#include <nng/protocol/reqrep0/req.h>
+
+#include "injection/interproc_coms.h"
 
 using namespace std;
 using namespace error_handling;
@@ -11,11 +13,14 @@ using CommandMessage = custom_ipc::CommandMessage;
 
 namespace custom_ipc {
 
-void ShowGUIMessage(std::string message) {
-  MessageBoxA(NULL, message.c_str(), "Injected process", MB_OK);
-}
-
 const char* kPipeName = R"(\\.\pipe\YourPipeName)";
+
+void ShowGUIMessage(string message) {
+  string window_title = "PID: " + to_string(GetCurrentProcessId());
+  thread([message, window_title]() {
+    MessageBoxA(NULL, message.c_str(), window_title.c_str(), MB_OK);
+  }).detach();
+}
 
 HANDLE WaitForMailSlot(string mailslot_name, DWORD timeout) {
   HANDLE mailslot;
@@ -254,7 +259,7 @@ ProgramResult NamedPipeServer::WaitForConnection() {
 
         if (waitResult != WAIT_OBJECT_0) {
           printf("Timeout");
-          // std::cerr << "Timed out waiting for client to connect." << std::endl;
+          // cerr << "Timed out waiting for client to connect." << endl;
           CloseHandle(overlapped.hEvent);
           return ErrorResult("Timed out while waiting for connection");
         }
@@ -287,12 +292,12 @@ ProgramResult NamedPipeServer::ServerLoop() {
     } else printf("Command successfully read\n");
 
     switch(cmd.command) {
-      case SEND_KEY_CMD:
+      case command::kExportKey:
         printf("Received key command %08X\n", cmd.first_arg);
         // MessageBoxA(NULL, "Received key command", "Yes, injected!", MB_OK);
         SendKey(static_cast<ULONG_PTR>(cmd.first_arg));
         break;
-      case END_SERVER_CMD:
+      case command::kEndServer:
         // MessageBoxA(NULL, "Received end command", "Yes, injected!", MB_OK);
         printf("Exiting via command\n");
         return OkResult("Exited via command");
@@ -436,7 +441,7 @@ ProgramResult NamedPipeClient::SendKeyHandle(ULONG_PTR key_handle) {
   ProgramResult func_result = OkResult("Key handle successfully sent");
 
   CommandMessage cmd;
-  cmd.command = SEND_KEY_CMD;
+  cmd.command = command::kExportKey;
   cmd.first_arg = (WPARAM) key_handle;
 
   ProgramResult pr = WriteMessage(reinterpret_cast<BYTE*>(&cmd), sizeof(CommandMessage));
@@ -533,7 +538,7 @@ ProgramResult NamedPipeClient::SendStopSignal() {
   ProgramResult func_result = OkResult("Stop signal successfully sent");
 
   CommandMessage cmd;
-  cmd.command = END_SERVER_CMD;
+  cmd.command = command::kEndServer;
   cmd.first_arg = 0;
 
   ProgramResult pr = WriteMessage(reinterpret_cast<BYTE*>(&cmd), sizeof(CommandMessage));
@@ -550,6 +555,225 @@ ProgramResult NamedPipeClient::CloseConnection() {
   }
 
   return func_result;
+}
+
+ProgramResult CustomEndpoint::SendMessage(const vector<BYTE> &buffer) {
+  if (!is_initialized_) return ErrorResult("Socket is not initialized (server invalid or not started)");
+
+  nng_msg* msg = nullptr;
+  int nng_res = nng_msg_alloc(&msg, buffer.size());
+  if (nng_res != 0) return ErrorResult("Failed to allocate message");
+
+  memcpy(nng_msg_body(msg), buffer.data(), buffer.size());
+
+  nng_res = nng_sendmsg(socket_, msg, 0);
+  if (nng_res != 0) {
+    nng_msg_free(msg);
+    return ErrorResult("Failed to send message: " + string(nng_strerror(nng_res)));
+  }
+
+  return OkResult("Message sent successfully");
+}
+
+ProgramResult CustomEndpoint::ReceiveMessage(vector<BYTE>& out_buffer) {
+  if (!is_initialized_) return ErrorResult("Socket is not initialized");
+
+  nng_msg* msg = nullptr;
+  int nng_res = nng_recvmsg(socket_, &msg, 0);
+  if (nng_res != 0) return ErrorResult("Failed to receive message: " + string(nng_strerror(nng_res)));
+
+  size_t recv_len = nng_msg_len(msg);
+  BYTE* msg_body = static_cast<BYTE*>(nng_msg_body(msg));
+  
+  out_buffer.assign(msg_body, msg_body + recv_len);
+  
+  nng_msg_free(msg);
+  return OkResult("Message received successfully");
+}
+
+void CustomEndpoint::Close() {
+  if (is_initialized_) {
+    nng_close(socket_);
+    is_initialized_ = false;
+  }
+}
+
+CustomEndpoint::CustomEndpoint()
+    : socket_name_(kGenericSocketName + to_string(GetCurrentProcessId())), 
+      is_initialized_(false) {}
+
+CustomClientV2::CustomClientV2(unsigned int timeout_millis) 
+    : CustomEndpoint(), timeout_millis_(timeout_millis) {}
+
+int CustomClientV2::OpenSocket() {
+  return nng_req_open(&socket_);
+}
+
+ProgramResult CustomClientV2::StartClient() {
+  if (is_initialized_) return OkResult("Client already started");
+
+  int nng_res = OpenSocket();
+  if (nng_res != 0) return ErrorResult("Failed to open client socket: " + string(nng_strerror(nng_res)));
+
+  if ((nng_res = nng_dialer_create(&dialer_, socket_, socket_name_.c_str())) != 0) {
+    nng_close(socket_);
+    return ErrorResult("Failed to create dialer: " + string(nng_strerror(nng_res)));
+  }
+
+  nng_setopt_ms(socket_, NNG_OPT_RECVTIMEO, timeout_millis_);  
+  nng_setopt_ms(socket_, NNG_OPT_SENDTIMEO, timeout_millis_);  
+  nng_setopt_int(socket_, NNG_OPT_REQ_RESENDTIME, 2000);  
+
+  // Start dialer in non-blocking mode
+  if ((nng_res = nng_dialer_start(dialer_, NNG_FLAG_NONBLOCK)) != 0) 
+    return ErrorResult("Failed to start dialer: " + string(nng_strerror(nng_res)));
+
+  is_initialized_ = true;
+  return OkResult("Client started successfully");
+}
+
+ProgramResult CustomClientV2::SendRequest(Request req) {
+  return SendMessage(req.serialize());
+}
+
+ProgramResult CustomClientV2::GetResponse(Response &res) {
+  vector<BYTE> buffer;
+  auto status = ReceiveMessage(buffer);
+  if (status.IsErr()) return status;
+
+  res.deserialize_here(buffer);
+  return OkResult("Response deserialized");
+}
+
+CustomServerV2::CustomServerV2() 
+    : CustomEndpoint() {}
+
+int CustomServerV2::OpenSocket() {
+  return nng_rep_open(&socket_);
+}
+
+ProgramResult CustomServerV2::SendResponse(Response res) {
+  return SendMessage(res.serialize());
+}
+
+ProgramResult CustomServerV2::StartServer() {
+  if (is_initialized_)
+    return OkResult("Server already initialized");
+
+  int nng_res = nng_rep_open(&socket_);
+  if (nng_res != 0)
+    return ErrorResult("Failed to open reply socket: " + string(nng_strerror(nng_res)));
+
+  nng_res = nng_listen(socket_, socket_name_.c_str(), nullptr, 0);
+  if (nng_res != 0) {
+    if (nng_res == NNG_EADDRINUSE) {
+      cout << "SERVER: Another server is already running on " 
+           << socket_name_ << ". Stopping.\n";
+      nng_close(socket_);
+      is_initialized_ = false;
+      return ErrorResult("Another server is already running. Stopping.");
+
+    } else {
+      nng_close(socket_);
+      return ErrorResult("Failed to listen on socket: " + string(nng_strerror(nng_res)));
+    }
+  }
+
+  is_initialized_ = true;
+  return OkResult("Server successfully started");
+}
+
+ProgramResult CustomServerV2::GetRequest(Request &req) {
+  vector<BYTE> buffer;
+  auto res = ReceiveMessage(buffer);
+  if (res.IsErr()) return res;
+
+  req.deserialize_here(buffer);
+  return OkResult("Data deserialized");
+}
+
+vector<unsigned char> Request::serialize() const {
+  vector<unsigned char> buffer(sizeof(command) + sizeof(SIZE_T) + data.size());
+
+  SIZE_T data_size = data.size();
+  memcpy(buffer.data(), &command, sizeof(command));
+  memcpy(buffer.data() + sizeof(command), &data_size, sizeof(data_size));
+  memcpy(buffer.data() + sizeof(command) + sizeof(data_size), data.data(), data_size);
+
+  return buffer;
+}
+
+Request Request::deserialize(const vector<unsigned char> &buffer) {
+  Request req;
+  SIZE_T data_size;
+  
+  memcpy(&req.command, buffer.data(), sizeof(req.command));
+  memcpy(&data_size, buffer.data() + sizeof(req.command), sizeof(data_size));
+  
+  req.data.resize(data_size);
+  memcpy(req.data.data(), buffer.data() + sizeof(req.command) + sizeof(data_size), data_size);
+
+  return req;
+}
+void Request::deserialize_here(const std::vector<unsigned char> &buffer) {
+}
+
+vector<unsigned char> Response::serialize() const
+{
+    vector<unsigned char> buffer(sizeof(code) + sizeof(SIZE_T) + data.size());
+
+    SIZE_T data_size = data.size();
+    memcpy(buffer.data(), &code, sizeof(code));
+    memcpy(buffer.data() + sizeof(code), &data_size, sizeof(data_size));
+    memcpy(buffer.data() + sizeof(code) + sizeof(data_size), data.data(), data_size);
+
+    return buffer;
+}
+
+Response Response::deserialize(const vector<unsigned char> &buffer) {
+  Response res;
+  SIZE_T data_size;
+  
+  memcpy(&res.code, buffer.data(), sizeof(res.code));
+  memcpy(&data_size, buffer.data() + sizeof(res.code), sizeof(data_size));
+  
+  res.data.resize(data_size);
+  memcpy(res.data.data(), buffer.data() + sizeof(res.code) + sizeof(data_size), data_size);
+
+  return res;
+}
+
+void Response::deserialize_here(const std::vector<unsigned char> &buffer) {
+  SIZE_T data_size;
+
+  memcpy(&code, buffer.data(), sizeof(code));
+  memcpy(&data_size, buffer.data() + sizeof(code), sizeof(data_size));
+  
+  data.resize(data_size);
+  memcpy(data.data(), buffer.data() + sizeof(code) + sizeof(data_size), data_size);
+}
+
+vector<unsigned char> KeyDataMessage::serialize() const {
+  auto buffer = vector<unsigned char>(sizeof(KeyDataMessage));
+
+  memcpy(buffer.data(), &key_handle, sizeof(key_handle));
+  memcpy(buffer.data() + sizeof(key_handle), &blob_type, sizeof(blob_type));
+
+  return buffer;
+}
+
+KeyDataMessage KeyDataMessage::deserialize(const vector<unsigned char> &buffer) {
+  KeyDataMessage key_data;
+
+  memcpy(&key_data.key_handle, buffer.data(), sizeof(key_handle));
+  memcpy(&key_data.blob_type, buffer.data() + sizeof(key_handle), sizeof(blob_type));
+
+  return key_data;
+}
+
+void KeyDataMessage::deserialize_here(const std::vector<unsigned char> &buffer) {
+  memcpy(&key_handle, buffer.data(), sizeof(key_handle));
+  memcpy(&blob_type, buffer.data() + sizeof(key_handle), sizeof(blob_type));
 }
 
 } // namespace custom_ipc
